c3e262f652b0f2648b520e6ab4964a8298c9a134
Clairebis <113165797+Clairebis@users.noreply.github.com>
2022-12-14 13:58:48 +0100

    CSS index

---
+++ js/index.js
@@ -0,0 +1,7 @@
+//modal window - newsletter
+const modal2Main = document.getElementById("myModal2Main");
+const btn2Main = document.getElementById("subscribeNewsletterMain");
+
+btn2Main.onclick = function () {
+  modal2Main.style.display = "block";
+};
