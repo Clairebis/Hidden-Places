b25783e02fbd81a43ef6197c717e56694cab038a
Clairebis <113165797+Clairebis@users.noreply.github.com>
2022-12-15 11:44:20 +0100

    JS weather

--- js/jeksen.js
+++ js/jeksen.js
@@ -1,3 +1,22 @@
 //Video controls
 var vid = document.getElementById("jeksenVid");
 vid.controls = true;
+
+//Weather url
+const weatherApiUrl =
+  "http://api.openweathermap.org/data/2.5/weather?q=Jeksen&lang=en&units=metric&appid=4969d2cea5e00ba3e5d664ee8fb7ac33";
+
+// fetch weather sample
+fetch(weatherApiUrl)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+
+    result.innerHTML += data.main.temp;
+    result2.innerHTML += data.main.feels_like;
+    result3.innerHTML += data.wind.speed;
+    result4.innerHTML = `<img src="http://openweathermap.org/img/wn/${data.weather[0].icon}.png" width="50px" height="50px" alt="">`;
+    // result4.innerHTML = '<img src="http://openweathermap.org/img/wn/' + data.weather[0].icon + '.png" width="50px" height="50px" alt="">';
+  });
